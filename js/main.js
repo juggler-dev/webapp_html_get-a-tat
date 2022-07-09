079038d6868b8a69ef88a834d4c23d6137338769
diego_s <dsantacruzmeoo00@mylangara.ca>
2022-07-08 18:02:45 -0700

    Final Structure

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, signInWithRedirect } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js';
+import { getFirestore, collection, addDoc, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw0RBWZgTpqCkdH6B3oWhU9rXjpz7-7Bw",
+  authDomain: "juggler-test-project.firebaseapp.com",
+  projectId: "juggler-test-project",
+  storageBucket: "juggler-test-project.appspot.com",
+  messagingSenderId: "1082104770953",
+  appId: "1:1082104770953:web:cbef63c511a7248aa3b158",
+  measurementId: "G-0CMQ3R7X0X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
+const providerTwo = new FacebookAuthProvider();
