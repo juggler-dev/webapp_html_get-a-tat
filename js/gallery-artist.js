cab4e0d10d108bb8ed04dfecb5b3366cddf7ed36
diego_s <dsantacruzmeoo00@mylangara.ca>
2022-07-12 19:08:59 -0700

    Added Gallery Artist Feature

---
+++ js/gallery-artist.js
@@ -0,0 +1,80 @@
+import { auth, storage, db } from "./main.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-storage.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, signInWithRedirect, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js';
+import { collection, doc, getDoc, addDoc, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+
+// Variables =====================
+let userUID = "";
+
+function drawNewTableRow(imgName, row){
+    let thumbnail = row.insertCell(0);
+    let name = row.insertCell(1);
+
+    //Thumbnail calling
+    const imageRef = ref(storage, userUID+ '/' +imgName);
+
+    getDownloadURL(imageRef)
+    .then((url) => {
+        thumbnail.innerHTML += `<img src='${url}' class="thumbnail-image"></img>`
+    })
+
+    //Name
+    name.innerHTML = `<p>${imgName}</p>`;
+}
+
+async function updateTable(collectionName) {
+    //Table Name
+    galleryTable.innerHTML = "";
+
+    const artistCollection = await getDocs(collection(db, collectionName));
+    artistCollection.forEach((doc) => {
+        drawNewTableRow(doc.data().img_name, galleryTable.insertRow(0))
+    });
+}
+
+// Events
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        // const uid = user.uid;
+        console.log('user logged in', user)
+        // console.log(user.uid);
+        // console.log(user.email);
+        userUID = user.uid;
+
+        // ...
+    } else {
+        // User is signed out
+        // ...
+
+        console.log('user logged out');
+    }
+});
+
+updateTable("artist_img_uploads");
+
+document.getElementById('addImageToGallery').addEventListener('click', async () => {
+
+    //Store in Storage
+    const userFile = document.getElementById('addImageArtist').files[0];
+    const filename = Date() + '.png';
+    const storegeRef = ref(storage, userUID + '/' + filename);
+
+    uploadBytes(storegeRef, userFile).then((snapshot) => {
+        console.log(snapshot);
+    });
+
+    console.log("Added");
+
+    //Store in Firestore
+
+    const docRef = await addDoc(collection(db, "artist_img_uploads"), {
+        artist_id: userUID,
+        img_name: filename
+    });
+
+// refreshGalleryList("artist_img_uploads");
+
+})
