964389b2edd721dec63fea7b67f8134d64ff79da
diego_s <dsantacruzmeoo00@mylangara.ca>
2022-07-25 10:16:35 -0700

    Modal partially working - appointment-management

--- js/appointment-management-client.js
+++ js/appointment-management-client.js
@@ -1,12 +1,14 @@
 ////////////////// IMPORTS //////////////////
 import { db, storage } from "./firebase-init.js";
-import { CLIENTS_COLLECTION_REFERENCE, ARTISTS_COLLECTION_REFERENCE } from "./firestore-references.js";
-import { addDoc, doc, setDoc, getDoc, getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+import { REQUEST_APPOINTMENTS_COLLECTION_REFERENCE } from "./firestore-references.js";
+import { addDoc, doc, setDoc, updateDoc, getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-storage.js";
 
 import { readSessionUserData } from "./session-storage.js";
 
 ////////////////// VARIABLES AND CONSTANTS //////////////////
+
+let bookingArray = [];
 
 let modal = document.getElementById("myModal");
 
@@ -23,57 +25,103 @@ const UID_FIELD = "uid";
 
 const STORAGE_FOLDER = "/appointments-img"
 
+const APPOINTMENT_BUTTON_CLASS = ".appointment-card-btn"
+
+////////////////// CLASSES //////////////////
+
+class Appointment {
+  constructor(id, image, artist, placement, size, allergies, description, color, date) {
+    this.id = id,
+      this.image = image,
+      this.artist = artist,
+      this.placement = placement,
+      this.size = size,
+      this.allergies = allergies,
+      this.description = description,
+      this.color = color,
+      this.date = date
+  }
+}
+
 ////////////////// FUNCTIONS //////////////////
 
+function buildAppointment(firebaseDocument) {
+  return new Appointment(firebaseDocument.id, firebaseDocument.data().image, firebaseDocument.data().artist, firebaseDocument.data().placement, firebaseDocument.data().size, firebaseDocument.data().allergies, firebaseDocument.data().description, firebaseDocument.data().color, firebaseDocument.data().date)
+}
 
-function myName() {
-  console.log("hello hello");
+function loadAppointmentDetail(appointmentObject) {
+  document.getElementById("modalPlacementInput").value = appointmentObject.placement;
+  console.log('??')
+  document.getElementById("modalSizeInput").value = appointmentObject.size;
+  document.getElementById("modalAllergiesInput").value = appointmentObject.allergies;
+  // // document.getElementById("modalDescriptionInput").value = appointmentObject.description;
+  // document.getElementById("modalColorInput").value = appointmentObject.color;
+  // document.getElementById("modalImageReference").src = appointmentObject.placement;
+}
+
+// async function retrieveAppointmentsFromFirestore(collectionName, sessionUserUID) {
+//   // Query
+//   const appointmentsUserQuery = query(collection(db, collectionName), where(UID_FIELD, "==", sessionUserUID));
+//   const userAppointments = await getDocs(appointmentsUserQuery);
+//   userAppointments.forEach((doc) => {
+
+//     const appointmentObject = buildAppointment(doc);
+//     bookingArray.push(appointmentObject);
+//   });
+// }
+
+function setButtonsEvents(querySelectorClass) {
+
+  document.querySelectorAll(querySelectorClass).forEach((button) => {
+    button.addEventListener('click', (e) => {
+      loadAppointmentDetail(bookingArray.find(appointment => appointment.id === e.target.id))
+      modal.style.visibility = "visible";
+      document.querySelector(".content").style.overflow = "hidden";
+    })
+  });
 }
 
 //Function to draw Rows in the table with the data from Firestore
-function drawAppointmentCard(photoName, imageName, artistName, appoinmentDate, container) {
-  // let thumbnail = row.insertCell(0);
-  // let artist = row.insertCell(1);
-  // let date = row.insertCell(2);
+function drawAppointmentCard(appointmentObject, container) {
 
-  
   let thumbnailElement;
   let artistElement;
   let dateElement;
   let artistDateGrouping;
-  let cardElement;
 
-  if (photoName !== ""){
-    thumbnailElement = `<img src='${photoName}' class="thumbnail-image"></img>`;
+  if (appointmentObject.image !== "") {
+    container.innerHTML += `<button class="appointment-card-btn" id="${appointmentObject.id}"></button>`;
 
-    artistElement = `<p>Appointment with: ${artistName}</p>`;
-    dateElement = `<p>Date: ${appoinmentDate}</p>`;
+    thumbnailElement = `<img src='${appointmentObject.image}' class="thumbnail-image"></img>`;
+
+    artistElement = `<p>Appointment with: ${appointmentObject.artist}</p>`;
+    dateElement = `<p>Date: ${appointmentObject.date}</p>`;
 
     artistDateGrouping = `<div class="appointment-info">${artistElement + dateElement}</div>`;
 
-    cardElement = `<button class="appointment-card-btn" >${thumbnailElement + artistDateGrouping}</button>`;
-
-    container.innerHTML += cardElement;
+    document.getElementById(appointmentObject.id).innerHTML += thumbnailElement + artistDateGrouping;
 
   }
 
   else {
-      //Thumbnail call
-  const thumbnailRef = ref(storage, STORAGE_FOLDER + '/' + imageName);
-  getDownloadURL(thumbnailRef)
-    .then((url) => {
-      // thumbnail.innerHTML += `<img src='${url}' class="thumbnail-image"></img>`;
-      thumbnailElement = `<img src='${url}' class="thumbnail-image"></img>`;
 
-      artistElement = `<p>Appointment with: ${artistName}</p>`;
-      dateElement = `<p>Date: ${appoinmentDate}</p>`;
+    container.innerHTML += `<button class="appointment-card-btn" id="${appointmentObject.id}"></button>`;
 
-      artistDateGrouping = `<div class="appointment-info">${artistElement + dateElement}</div>`;
+    //Thumbnail call
+    const thumbnailRef = ref(storage, STORAGE_FOLDER + '/' + appointmentObject.id);
+    getDownloadURL(thumbnailRef)
+      .then((url) => {
+        thumbnailElement = `<img src='${url}' class="thumbnail-image"></img>`;
 
-      cardElement = `<button class="appointment-card-btn">${thumbnailElement + artistDateGrouping}</button>`;
+        artistElement = `<p>Appointment with: ${appointmentObject.artist}</p>`;
+        dateElement = `<p>Date: ${appointmentObject.date}</p>`;
 
-      container.innerHTML += cardElement;
-    })
+        artistDateGrouping = `<div class="appointment-info">${artistElement + dateElement}</div>`;
+
+        // cardElement = `<button class="appointment-card-btn" id="${appointmentObject.id}">${thumbnailElement + artistDateGrouping}</button>`;
+
+        document.getElementById(appointmentObject.id).innerHTML += thumbnailElement + artistDateGrouping;
+      })
   }
 
 }
@@ -84,44 +132,95 @@ async function updateAppointmentList(collectionName, sessionUserUID) {
   const appointmentsUserQuery = query(collection(db, collectionName), where(UID_FIELD, "==", sessionUserUID));
   const userAppointments = await getDocs(appointmentsUserQuery);
   userAppointments.forEach((doc) => {
-    drawAppointmentCard(doc.data().image, doc.id, doc.data().artist, doc.data().date, document.getElementById('appointmentContainer'))
+
+    const appointmentObject = buildAppointment(doc);
+    bookingArray.push(appointmentObject);
+    drawAppointmentCard(appointmentObject, document.getElementById('appointmentContainer'));
   });
+}
+
+function renderTable() {
+
 }
 
 ////////////////// EVENTS //////////////////
 
-// console.log(document.querySelectorAll(".buttonToTest"));
-
 //Read Session Object
 const sessionObject = readSessionUserData(SESSION_USER_KEY_VALUE);
+
+// //Pull all appointments from Firebase.
+// await retrieveAppointmentsFromFirestore(COLLECTION_NAME, sessionObject.uid);
 
 //Update table with content
 await updateAppointmentList(COLLECTION_NAME, sessionObject.uid);
 
-document.querySelectorAll(".appointment-card-btn").forEach((button) => {
+//Sets click events to appointment buttons
+setButtonsEvents(".appointment-card-btn");
 
-  button.addEventListener('click', () => {
-    modal.style.visibility = "visible";
-  })
-})
 
 ////MODAL EVENTS
+document.getElementById('modalContentForm').addEventListener('submit', (e) => {
+
+  e.preventDefault();
+})
 
 document.getElementById('closingX').addEventListener('click', () => {
   modal.style.visibility = "hidden";
+  document.querySelectorAll(".modal-input").forEach((input) => {
+    input.disabled = true;
+    input.classList.remove("modal-input-enabled")
+  })
 })
 
-// When the user clicks on <span> (x), close the modal
-window.addEventListener('click', (event) => {
-  if (event.target == modal) {
-    modal.style.visibility = "hidden";
-  }
+// // When the user clicks on <span> (x), close the modal
+// window.addEventListener('click', (event) => {
+//   if (event.target == modal) {
+//     modal.style.visibility = "hidden";
+//   }
+// })
+
+//Edit Button
+document.getElementById("modalContentBtnEdit").addEventListener('click', async (e) => {
+
+  // e.target.innerHTML = "Update";
+  document.querySelectorAll(".modal-input").forEach((input) => {
+    input.disabled = false;
+    input.classList.add("modal-input-enabled")
+  });
+
+
 })
 
+//Update Button
+document.getElementById("modalContentBtnUpdate").addEventListener('click', async (e) => {
 
-//Move to Create Appointments page
+
+  const appointmentToUpdateRef = doc(db, "request_appointment", "rhSdLpG5VKWwH0KXisDp");
+  await updateDoc(appointmentToUpdateRef, {
+    placement: document.getElementById('modalPlacementInput').value,
+    size: document.getElementById('modalSizeInput').value,
+    allergies: document.getElementById('modalAllergiesInput').value
+    // description: document.getElementById('short-description').value,
+    // color: document.getElementById('color').value,
+    // date: document.getElementById('datePicker').value,
+    // time: document.getElementById('timePicker').value,
+    // uid: readSessionUserData(SESSION_USER_KEY_VALUE).uid,
+    // image: document.getElementById('image').value,
+  })
+  location.reload();
+})
+
+//Cancel Button
+document.getElementById("modalContentBtnCancel").addEventListener('click', (e) => {
+  modal.style.visibility = "hidden";
+  document.querySelectorAll(".modal-input").forEach((input) => {
+    input.disabled = true;
+    input.classList.remove("modal-input-enabled")
+  })
+})
+
+////Move to Create Appointments page
 document.getElementById('createAppoinmentBtn').addEventListener('click', () => {
-
   window.location.href = "../pages/book-appointment.html";
 });
 
