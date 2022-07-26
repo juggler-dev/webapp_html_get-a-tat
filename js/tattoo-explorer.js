6f29114e35fa2d0c07c767e4317ec4bb26940e1e
dlaracarvajal <d.lara.carvajal@gmail.com>
2022-07-26 12:33:24 -0700

    tattoo explorer working partially

--- js/tattoo-explorer.js
+++ js/tattoo-explorer.js
@@ -6,6 +6,7 @@ import { collection, doc, getDoc, addDoc, query, where, getDocs } from "https://
 
 //Variables
 const artistUIDArray = [];
+const artistNameArray = [];
 
 //Get all artists uid from DB
 const artists = await getDocs(ARTISTS_COLLECTION_REFERENCE);
@@ -14,9 +15,16 @@ artistUIDArray.push(artist.data().uid)
 })
 console.log(artistUIDArray);
 
+//Get all artists name from DB
+const artistsName = await getDocs(ARTISTS_COLLECTION_REFERENCE);
+artistsName.forEach((artist) => {
+artistNameArray.push(artist.data().full_name, artist.data().uid)
+})
+console.log(artistNameArray);
+
 
 //Get all images from storage
-artistUIDArray.forEach((artist)=>{
+artistNameArray.forEach((artist)=>{
   console.log(artist);
   const imagesListRef = ref(storage, `artist-img-uploads/${artist}`);
   listAll(imagesListRef)
@@ -27,9 +35,37 @@ artistUIDArray.forEach((artist)=>{
 );
   getDownloadURL(pathReference)
     .then((url) => {
-      imageDisplay.innerHTML += `<img src='${url}'>`
-  })
+     imageDisplay.innerHTML += `<img src='${url}' alt=''>`
+ 
+    const imagesExplorer = document.querySelectorAll(`.imagesExplorer img`);
+    const modalExplorer = document.querySelector(".modalExplorer");
+    const modalImgExplorer = document.querySelector(".modalImgExplorer");
+    const modalTxtExplorer = document.querySelector(".modalTxtExplorer");
+    const closeExplorer = document.querySelector(".closeModalExplorer");
+
+
+    imagesExplorer.forEach((image) => {
+    image.addEventListener("click", () => {
+      // modalTxtExplorer.innerHTML = image.alt;
+      modalExplorer.classList.add("appear");
+      console.log('open');
+      modalImgExplorer.src = image.src;
+      // modalTxtExplorer.innerHTML = image.alt;
+      ctaBtn.innerHTML = `
+      <h3>Tatto made by</h3>
+      <p><b>Artist name</b></p>
+      <p>@instagram</p>
+      <a href="../pages/book-appointment.html"><button>Bio</button></a>
+      `
     });
+    closeExplorer.addEventListener("click", () => {
+      modalExplorer.classList.remove("appear");
+      // console.log('closed');
+
+    });
+      });
+        })
+         });
   }).catch((error) => {
     console.log("Error folderRef");
     // Uh-oh, an error occurred!
