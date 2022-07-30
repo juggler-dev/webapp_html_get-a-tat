9e54fc02bb32fcdff758b9b3f71b162f97444f08
alerayes <alerayes@hotmail.com>
2022-07-29 17:07:26 -0700

    profile icon working on all pages

--- js/profile-icon.js
+++ js/profile-icon.js
@@ -10,24 +10,46 @@ const USER_TYPE_ARTIST = "artist";
 
 
 
+// if (readSessionUserData(SESSION_USER_KEY_VALUE).user_type == 'artist'){
+//   console.log('user is logged in');
+
+//   const artistButton = document.getElementById("dropdownMenuButton");
+//   const anchorTag = document.getElementById("loginButtonAnchorTag");
+
+//   console.log(artistButton);
+//   console.log(anchorTag);
+
+//   const myIcon = document.createElement("img");
+//   const imgSrc = document.createAttribute("src");
+//   imgSrc.value = "../icons/user-icon.png";
+//   myIcon.setAttributeNode(imgSrc);
+//   anchorTag.appendChild
+
+
+// }
+
+
+
+
 if (readSessionUserData(SESSION_USER_KEY_VALUE).user_type == 'artist') {
   console.log('user is logged in');
   console.log(readSessionUserData(SESSION_USER_KEY_VALUE).user_type);
 
   const anchorTag = document.getElementById("loginButtonAnchorTag");
-  anchorTag.remove();
+  
   
 
-
+  if( typeof(anchorTag) != undefined && anchorTag != null){
+    anchorTag.remove();
+  }
+  
   const artistButton = document.getElementById("dropdownMenuButton");
+  console.log(artistButton);
 
   const myIcon = document.createElement("img");
   const imgSrc = document.createAttribute("src");
- 
   imgSrc.value = "../icons/user-icon.png";
-
   myIcon.setAttributeNode(imgSrc);
-
   artistButton.appendChild(myIcon);
 
   artistButton.addEventListener("click", () => {
@@ -66,15 +88,15 @@ if (readSessionUserData(SESSION_USER_KEY_VALUE).user_type == 'artist') {
   })
 
 
-
-
-
 } else if  (readSessionUserData(SESSION_USER_KEY_VALUE).user_type == 'client') {
   console.log('logged in');
   console.log(readSessionUserData(SESSION_USER_KEY_VALUE).user_type);
 
   const clientAnchorTag = document.getElementById("loginButtonAnchorTag");
-  clientAnchorTag.remove();
+  if( typeof(clientAnchorTag) != undefined && clientAnchorTag != null){
+    clientAnchorTag.remove();
+  }
+  
 
   const clientButton = document.getElementById("dropdownMenuButton");
   
@@ -127,93 +149,6 @@ if (readSessionUserData(SESSION_USER_KEY_VALUE).user_type == 'artist') {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-// if (readSessionUserData(SESSION_USER_KEY_VALUE) != null) {
-//   console.log("user.log")
-
-//   // const auth = getAuth();
-//   // const user = auth.currentUser;
-
-//   // db.collection("storie").where("author", "==", user.uid).get()
-//   // // onAuthStateChanged(auth, (user) => {
-//   // //  if (user) {
-//   // //   // User is signed in, see docs for a list of available properties
-//   // //   // https://firebase.google.com/docs/reference/js/firebase.User
-//   // //   const uid = user.uid;
-//   // //   console.log(uid);
-//   // //   const type = uid.user_type;
-//   // //   console.log(type);
-//   // //   // ...
-//   // // } else {
-//   // //   // User is signed out
-//   // //   // ...
-//   // // }
-// }
-
-  // const ARTISTS_COLLECTION_REFERENCE = collection(db, 'artists');
-
-
-  // if(artists.data.user_type == USER_TYPE_ARTIST) {
-  //   console.log("artist.log");
-  // }
-
-    // console.log("user logged in");
-
-    // const anchorTag = document.getElementById("loginButtonAnchorTag");
-    // anchorTag.remove();
-
-    // const myButton = document.getElementById("myLoginButton");
-    // const myIcon = document.createElement("img");
-    // const imgSrc = document.createAttribute("src");
-   
-    // imgSrc.value = "../icons/user-icon.png";
-
-    // myIcon.setAttributeNode(imgSrc);
-
-    // myButton.appendChild(myIcon);
-
-
-
-  //  else {
-  //   console.log("no user");
-  // }
-
-
-
-
-  // if (readSessionUserData(SESSION_USER_KEY_VALUE)) {
-  //   console.log("user logged in");
-
-    // const anchorTag = document.getElementById("loginButtonAnchorTag");
-    // anchorTag.remove();
-
-    // const artistButton = document.getElementById("dropdownMenuArtist");
-
-    // const myIcon = document.createElement("img");
-    // const imgSrc = document.createAttribute("src");
-   
-    // imgSrc.value = "../icons/user-icon.png";
-
-    // myIcon.setAttributeNode(imgSrc);
-
-    // artistButton.appendChild(myIcon);
-
-    
-
-
-
-  // } else {
-  //   console.log("no user");
-  // }
 
 
 
